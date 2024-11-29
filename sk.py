e8ef7173685ff1010a546e83a6c4ee3ac5cd4912
Ransford <ransfordaddai111@gmail.com>
2024-11-29 11:41:28 -0800

    fourth branch

--- sk.py
+++ sk.py
@@ -1,3 +1,4 @@
 print('This is first')
 print('This is first plus')
 print('Second Branch is coming')
+print('Fourthco coming')
--- skk.py
+++ skk.py
@@ -1,3 +1,4 @@
 print('This is second')
 print('This is second plus')
 print('Second branch is coming')
+print('fourthco coming')
